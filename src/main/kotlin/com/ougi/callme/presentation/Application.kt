d82b75f4dbf4bd8491bf12fc606eb11af015021e
comougi <comougi@gmail.com>
2024-04-24 20:31:21 +0300

    update port

--- src/main/kotlin/com/ougi/callme/presentation/Application.kt
+++ src/main/kotlin/com/ougi/callme/presentation/Application.kt
@@ -10,7 +10,7 @@ import org.koin.ktor.plugin.Koin
 fun main() {
     embeddedServer(
         factory = Netty,
-        port = 8080,
+        port = 8081,
         host = "0.0.0.0",
         module = Application::module
     )
